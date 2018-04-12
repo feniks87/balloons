6655869ebdea923a0b91ce34bb325a34922516b9
Kate Shaitarova <eshaytarova@gmail.com>
2018-04-12 21:46:25 +1200

    Add background music to the start screen

--- app/src/main/java/com/example/eshay/balloons/MainActivity.java
+++ app/src/main/java/com/example/eshay/balloons/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.eshay.balloons;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
@@ -24,10 +24,23 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(v.getContext(), GameActivity.class);
                 v.getContext().startActivity(intent);
+                Intent myIntent = new Intent(MainActivity.this, PlayMusicService.class);
+                startService(myIntent);
             }
         });
-
     }
 
-
+    @Override
+    public void onPause() {
+        super.onPause();
+        Intent myIntent = new Intent(MainActivity.this, PlayMusicService.class);
+        stopService(myIntent);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Intent myIntent = new Intent(MainActivity.this, PlayMusicService.class);
+        startService(myIntent);
+    }
+}
